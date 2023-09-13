4ea8d4ad98fb19433ae308c8b197a26bbaef0d94
caidenceus <109473462+caidenceus@users.noreply.github.com>
2023-09-12 22:30:42 -0500

    Update magnavox_tv.ino

--- ir/transmitter/magnavox_tv.ino
+++ ir/transmitter/magnavox_tv.ino
@@ -6,20 +6,26 @@ const int powerButton = 12;
 
 void setup() {
     Serial.begin(300);
+
+    // set pin 12 to listen for input from the button
     pinMode(powerButton, INPUT);
+
+    // Send IR data on pin 3
     IrSender.begin(3, true, USE_DEFAULT_FEEDBACK_LED_PIN);
 }
 
 
 void loop() {
-  int dataSent = 0;
+    int dataSent = 0;
 
-  while (1) {
-    delay(500);
+    while (1) {
+        // half a second
+        delay(500);
 
-    if (digitalRead(powerButton) == HIGH) {
-      dataSent = IrSender.write(RC5, 0, 12, 1);
-      Serial.print(dataSent);
+        if (digitalRead(powerButton) == HIGH) {
+            // 1 if data was sent, 0 otherwise
+            dataSent = IrSender.write(RC5, 0, 12, 1);
+            Serial.print(dataSent);
+        }
     }
-  }
 }
