c1b8247cfa58a8610faddc5519939de8f6c32bb3
caidenceus <caidentp@icloud.com>
2023-11-23 19:04:03 -0600

    Add basic low power code

--- ir/transmitter/universal_tv_remote.ino
+++ ir/transmitter/universal_tv_remote.ino
@@ -1,3 +1,4 @@
+#include <LowPower.h>
 #include <IRremote.h>
 
 const int irSendPin = 2;
@@ -7,8 +8,6 @@ const int volumeUpButton = 4;
 const int volumeDownButton = 5;
 const int channelUpButton = 6;
 const int channelDownButton = 7;
-
-const int appliancePowerButton = 12;
 
 
 void setup() {
@@ -20,37 +19,49 @@ void setup() {
     pinMode(channelUpButton, INPUT);
     pinMode(channelDownButton, INPUT);
 
-    pinMode(appliancePowerButton, INPUT);
-
     // Send IR data on pin irSendPin
     IrSender.begin(irSendPin, true, USE_DEFAULT_FEEDBACK_LED_PIN);
 }
 
+int timer;
+int modeCounter = 0;
+
+void wakeUp() {
+    timer = 0;
+}
 
 void loop() {
-    while (1) {
-        delay(500);
-
+    // attachInterrupt(0, wakeUp, HIGH);
+    while (true) {
         // (protocol, address, command, times to send)
         if (digitalRead(powerButton) == HIGH) {
-            int data = IrSender.write(RC5, 0, 12, 1);
-            Serial.println(data);
+            timer = 0;
+            delay(50);
+            IrSender.write(RC5, 0, 12, 1);
         }
         if (digitalRead(volumeUpButton) == HIGH) {
+            timer = 0;
+            delay(50);
             IrSender.write(RC5, 0, 16, 1);
         }
         if (digitalRead(volumeDownButton) == HIGH) {
+            timer = 0;
+            delay(50);
             IrSender.write(RC5, 0, 17, 1);
         }
         if (digitalRead(channelUpButton) == HIGH) {
+            timer = 0;
+            delay(50);
             IrSender.write(RC5, 0, 32, 1);
         }
         if (digitalRead(channelDownButton) == HIGH) {
+            timer = 0;
+            delay(50);
             IrSender.write(RC5, 0, 33, 1);
         }
-        if (digitalRead(appliancePowerButton) == HIGH) {
-          IrSender.sendSAMSUNG(0xE0E040BF, 32);
-          // IrSender.write(SAMSUNG, 45863, 4168);
-        }
+        delay(1);
+        timer += 1;
     }
+
+    // LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
 }
